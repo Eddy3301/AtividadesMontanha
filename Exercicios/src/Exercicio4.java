acb7a91b7bd1333893c3143aafe90e4195251715
Eduardo Luiz <eduardoluizdag@gmail.com>
2026-03-15 01:00:03 -0300

    Envio da atividade 4

---
+++ Exercicios/src/Exercicio4.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Exercicio4 {
+    public static void main(String[] args) {
+        double salario;
+        double novoSalario;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.printf("|| EMPRESA DO MONTANHA || \n");
+        System.out.printf("PARABÉNS PELA PROMOÇÃO! \n");
+        System.out.printf("Digite o seu salário atual: ");
+        salario = scanner.nextDouble();
+        novoSalario = salario + (salario * 7 / 100);
+        System.out.printf("O seu novo salário é: R$ %.2f", novoSalario);
+    }
+}
